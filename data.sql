b79372cac44d6042929c6756c975a5a7b0ea4fc1
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-02 23:14:14 +0500

    Updated the table and created queries

--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 10.23, true, 0), ('Gabumon', '2018-10-15', 8.00, true, 2), ('Pikachu', '2021-01-07', 15.04, false, 0), ('Devimon', '2017-05-12', 11.00, true, 5);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 10.23, true, 0), ('Gabumon', '2018-10-15', 8.00, true, 2), ('Pikachu', '2021-01-07', 15.04, false, 1), ('Devimon', '2017-05-12', 11.00, true, 5);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2020-02-08', -11, false, 0), ('Plantmon', '2021-10-15', -5.7, true, 2), ('Squirtle', '1993-04-02', -12.13, false, 3), ('Angemon', '2005-06-12', -45, true, 1), ('Boarmon', '2005-06-07', 20.4, true, 7), ('Blossom', '1998-10-13', 17, true, 3), ('Ditto', '2022-05-14', 22, true, 4);
