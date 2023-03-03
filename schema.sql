5e7b30b1235a54238d14e31bc0f7c0cf610e2cbc
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-04 01:50:18 +0500

    Added more tables and queries

--- schema.sql
+++ schema.sql
@@ -27,3 +27,20 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD owner_id int;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
 
+CREATE TABLE vets (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name varchar(100) NOT NULL,
+    age int NOT NULL,
+    date_of_graduation date NOT NULL
+);
+
+CREATE TABLE specializations (
+    species_id int REFERENCES species(id),
+    vet_id int REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animal_id int REFERENCES animals(id),
+    vet_id int REFERENCES vets(id),
+    date_of_visit date NOT NULL
+);
