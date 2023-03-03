1052de99c16b9f338aa0cd9a78a60954073fdb4e
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-03 22:33:28 +0500

    Added more tables and used JOIN for queries

--- schema.sql
+++ schema.sql
@@ -9,3 +9,21 @@ CREATE TABLE animals (
     weight_kg decimal NOT NULL
 );
 ALTER TABLE animals ADD species varchar(100) NOT NULL;
+
+CREATE Table owners (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    full_name varchar(100) NOT NULL,
+    age int NOT NULL
+);
+
+CREATE Table species (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name varchar(100) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id int;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id int;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
